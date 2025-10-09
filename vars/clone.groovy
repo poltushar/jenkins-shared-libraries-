0132fb4478bb0b1eadd44abda76216d17e9e7eb8
poltushar <127743959+poltushar@users.noreply.github.com>
2025-10-09 11:28:43 +0530

    Create clone.groovy

---
+++ vars/clone.groovy
@@ -0,0 +1,5 @@
+def call() {
+   echo "This is cloning the code"
+                git url:"https://github.com/LondheShubham153/django-notes-app.git",branch:"main"
+                echo "Code Cloning Successful"
+}
