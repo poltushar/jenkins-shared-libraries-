49991e71d173e30f6afb2950dcb2ea04c2f42030
poltushar <127743959+poltushar@users.noreply.github.com>
2025-12-03 13:45:20 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -6,7 +6,7 @@
 //    echo "Code Cloning Successful"
 // }
 
-def call(String repoUrl, String branch ) {
+def gitClone(String repoUrl, String branch ) {
     echo "Cloning repository: ${repoUrl}"
     // checkout([$class: 'GitSCM', 
     //           branches: [[name: "*/${branch}"]], 
